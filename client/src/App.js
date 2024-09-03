5f4ebf36339dafa10133782ae8a67fd8ad16f1b4
Your Name <you@example.com>
2024-09-03 11:24:37 +0500

    firstpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Carousel from './components/Carousel';
+import "./styles.css"
+
 function App() {
   return (
-  <>
-  <h1>Hello From Client</h1>
-  </>
+    <>
+   
+    <Header/>
+    <Nav/>
+     <Carousel/>
+     <div className='text' style={{textAlign:"center", color:"Black", fontFamily:"sans-serif", marginTop:"20px"  }}>
+<h2><b>Welcome to Shalamar Hospital</b></h2>
+<p style={{ fontSize:"15px" }}>Shalamar Hospital has a renowned history of serving the community since its founding in 1978. The hospital is equipped  with world-class <br/> medical diagnosis and treatment equipment. Established as a purpose-built building, Shalamar Hospital has grown to a 500-bed medical facility, offering medical services <br/> to the community and international standards, including the American Heart Association and the American College of Cardiology. Shalamar Hospital is committed to making healthcare <br/>accessible by ensuring that its services are patient-centered and delivered with excellence and empathy</p>
+</div>
+    </>
   );
 }
 
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div >
+    <ul class="bar">
+        <li class="grand8" id="Summer">Donate Now</li>
+        <li class="grand"> Patient Portal</li>
+        <li class="grand1">Careers
+        </li>
+        <li class="grand2">Book Appointment</li>
+        <li class="grand3">Women Apparels</li>
+       
+    </ul>
+    </div>
+  )
+}
+
+export default Nav
