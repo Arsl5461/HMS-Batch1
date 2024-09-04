4ab8f522793acf8c143341a25884bb8ee440a040
ayesha ali <@ayeshafatehali gmail.com>
2024-09-04 10:25:21 +0500

    added sidebar

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 const Dashboard = () => {
   return (
-    <div>Welcome Admin</div>
+    <div>
+        
+<html lang="en"/>
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Responsive Sidebar</title>
+    <link rel="stylesheet" href="styles.css"/>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="sidebar-content">
+            <a href="#">Home</a>
+            <a href="#">Doctors</a>
+            <a href="#">Timing</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>
+ 
+</body>
+<html/>
+
+
+
+    </div>
   )
+
+
 }
 
 export default Dashboard
