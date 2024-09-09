5512712eb8614644574cd926519c8e5bbdc22d56
Zunoon 89 <zunoonbhatti1@gmail.com>
2024-09-08 22:20:43 -0700

    contact us page added

---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import Header from '../components/Header'
+import ContactUs from '../components/ContactUs';
+import Footer from '../components/Footer'
+
+const Contact = () => {
+  return (
+    <>
+    <Header/>
+    <Nav/>
+    <ContactUs/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Contact
