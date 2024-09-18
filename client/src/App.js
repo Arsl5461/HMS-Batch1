68ae32730769ee0d096b38814b352023d827786d
Your Name <you@example.com>
2024-09-18 11:25:46 +0500

    adsd

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,9 @@ import Donate from './pages/Donate';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Parent from './components/Parent';
+import Vacent from './components/Vacent';
+import Program from './components/Program';
+
 
 
 
@@ -28,7 +31,9 @@ function App() {
       <Route path='/' element={<Home/>}></Route>
         <Route path='/Contact' element={<Contact/>}></Route>
         <Route path='/Donate' element={<Donate/>}></Route>
-               <Route path='/Parent' element={<Parent/>}></Route>
+        <Route path='/Parent' element={<Parent/>}></Route>
+        <Route path='/Vacent' element={<Vacent/>}></Route>
+        <Route path='/Program' element={<Program/>}></Route>
       </Routes>
       <Footer />
       <ToastContainer/>
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./nav.css"
+import { IoCaretDownOutline } from "react-icons/io5";
 
 const Nav = () => {
   return (
     <div className='index' >
-    <ul class="bar text-light " >
+      <ul class="bar text-light " >
         <li class="grand8 ">
-        <Link  id="Summer" to="/Donate" style={{color:"white",textDecoration:"none"}}>Donate Now</Link>
+          <Link id="Summer" to="/Donate" style={{ color: "white", textDecoration: "none" }}>Donate Now</Link>
         </li>
         <li class="grand ">
-        <Link  to="" style={{color:"white",textDecoration:"none"}}> Patient Portal</Link>
-        </li>
-        <li class="grand1 ">
-        <Link  to="" style={{color:"white",textDecoration:"none"}}> Careers</Link>
+          <Link to="" style={{ color: "white", textDecoration: "none" }}> Patient Portal</Link>
         </li>
         <li class="grand2 ">
-        <Link  to="" style={{color:"white",textDecoration:"none"}}> Book Appointment</Link>
+          <Link to="" style={{ color: "white", textDecoration: "none" }}> Book Appointment</Link>
         </li>
-    </ul>
+        <div class="dropdown">
+          <button class="dropbtn">Careers
+            <IoCaretDownOutline />
+          </button>
+          <div class="dropdown-content">
+      <Link to="/Vacent">Vacant Position</Link>
+      <Link to="/Program">PG Programs</Link>
+      <Link to="/">Apply Online</Link>
+    </div>
+        </div>
+      </ul>
     </div>
   )
 }
