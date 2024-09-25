aafe0a539dd6c366ae55bd5ef388d95ae36cdec5
Zain2418418 <zainulabedeen2418@gmail.com>
2024-09-25 11:48:15 +0500

    fetch api addded

--- Backend/controller/doctor.controller.js
+++ Backend/controller/doctor.controller.js
@@ -1,11 +1,35 @@
-const Doctor=require("../models/doctor.model")
+const Doctor = require("../models/doctor.model")
 
-exports.store=async(req,res)=>{
-    try{
-     const doctor=await Doctor.create(req.body);
-res.json({messsage:"Doctor Created Successfully",status:200,success:true,doctor})
+exports.store = async (req, res) => {
+    try {
+        const doctor = await Doctor.create(req.body);
+        res.json({ messsage: "Doctor Created Successfully", status: 200, success: true, doctor })
     }
-    catch(err){
+    catch (err) {
+        console.log(err)
+    }
+}
+
+exports.index = async (req, res) => {
+    try {
+        const doctors = await Doctor.find();
+        return res.json({ message: "Doctors Fetch Successfully", status: 200, success: true, doctors })
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+exports.get = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const doctor = await Doctor.findOne({ _id: id })
+        if (!doctor) {
+            return res.json({ message: "Doctor Not Found", status: 404, success: false })
+        }
+        return res.json({ message: "Doctor Fetched Successfully!", status: 200, success: true, doctor })
+    }
+    catch (err) {
         console.log(err)
     }
 }
--- Backend/routes/doctor.route.js
+++ Backend/routes/doctor.route.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const controller=require("../controller/doctor.controller")
 
 router.post("/",controller.store)
-
+router.get("/",controller.index)
+router.get("/:id",controller.get)
 
 module.exports=router
