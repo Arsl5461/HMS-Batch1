a387e486fb00efc63ddc0222f65488eef7abc3c4
Zain2418418 <zainulabedeen2418@gmail.com>
2024-09-30 10:57:41 +0500

    delete and update api created

--- Backend/routes/doctor.route.js
+++ Backend/routes/doctor.route.js
@@ -5,5 +5,9 @@ const controller=require("../controller/doctor.controller")
 router.post("/",controller.store)
 router.get("/",controller.index)
 router.get("/:id",controller.get)
+router.delete("/:id",controller.destroy)
+router.put("/:id",controller.update)
+
+
 
 module.exports=router
