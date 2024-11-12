078a9d7a1e55be47df80c3f9217d57acf89820d3
Uzair Dhillon <uzairdhillonuzair@gmail.com>
2024-11-11 22:01:44 -0800

    updated doctors

--- Backend/config/connectDb.js
+++ Backend/config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose =require("mongoose")
 
 const connectDb=async()=>{
     try{
-await mongoose.connect("mongodb://localhost:27017/HMS")
+await mongoose.connect("mongodb://localhost:27017")
 console.log("MongoDb Connected Successfully");
     }
     catch(err){
